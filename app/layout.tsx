26f6275d3490df0e36ee3915c952ebb1135a289e
Vishakh Abhayan <vishakhabhayan0@gmail.com>
2024-06-27 07:10:04 +0530

    feat:Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { IBM_Plex_Sans } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react";
 
 import { ClerkProvider } from "@clerk/nextjs";
 
@@ -24,6 +25,7 @@ export default function RootLayout({
 }>) {
   return (
     <ClerkProvider appearance={{ variables: { colorPrimary: "#624cf5" } }}>
+      <Analytics />
       <html lang="en">
         <body className={cn("font-IBMPlex antialiased", IBMPLex.variable)}>
           {children}
